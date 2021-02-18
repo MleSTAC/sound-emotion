633f2891889f97a346479067c9c451bae7e8306f
MleSTAC <78829273+MleSTAC@users.noreply.github.com>
2021-02-19 10:38:01 +1300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    soundExpression.hello.play()
+})
+basic.forever(function () {
+	
+})
